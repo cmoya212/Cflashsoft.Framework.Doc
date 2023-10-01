218b31b907c9287481f1dc26c0668d9104d2baf4
Carlos Moya <cmoya212@gmail.com>
2023-10-01 11:28:04 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+
+var cfxDocConfig = {
+    xmlDocs: [
+        //add your Visual Studio xml doc files here
+        "xmldocs/Cflashsoft.Framework.Http.xml",
+        "xmldocs/Cflashsoft.Framework.SecurityCore.xml",
+        "xmldocs/Cflashsoft.Framework.Data.xml",
+        "xmldocs/Cflashsoft.Framework.Logging.xml",
+        "xmldocs/Cflashsoft.Framework.Optimization.xml"    
+    ]
+};
